2fcf6b7f0120de2c41de88bcd50c91e5311427a3
Andres Quan <aquan@lanl.gov>
2019-10-28 11:32:26 -0600

    Data is being populated but not displaying

--- cinema/bandit/1.1/js/HeatMapDisplay.js
+++ cinema/bandit/1.1/js/HeatMapDisplay.js
@@ -46,7 +46,143 @@ function HeatMapDisplay(parent, getData) {
 	this.mainContext = this.mainCanvas.node().getContext('2d');
 	this.mainCSV;
 
+	this.create = function() {
+		var myGroups;
+		var myVars;
+		var csvFile = this.getData(0, "FILE_heatmap_path")[0];
+		d3.csv(csvFile, function(data){
+		myGroups = d3.map(data, function(d){
+			return d.raw_scattering_angle;
+		}).keys();
+		myVars = d3.map(data, function(d){
+			return d.y_position;
+		}).keys();
+
+		// Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+
+		this.heatMapWrapper = this.displays[5].heatMapWrapper;
+		var parentContext = this.displays[5];
+
+		var width = parentContext.internalWidth;
+		var height = parentContext.internalHeight;
+		var margin = parentContext.margin;
+			
+			// Build X scales and axis:
+		var x = d3.scaleBand()
+		    .range([ 0, width ])
+		    .domain(myGroups)
+		    .padding(0.00);
+		    heatMapWrapper.append("g")
+		    .style("font-size", 15)
+		    .attr("transform", "translate(0," + height + ")")
+		    //keep every hundredth tick mark, and include the first and last.
+		    .call(d3.axisBottom(x).tickValues(x.domain().filter(function(d,i){return !(i%100) || i === x.domain().length - 1})).tickSize(6))
+		    .select(".domain").remove();
+		    heatMapWrapper.append("text")
+		    .attr("transform",
+			"translate(" + (width/2) + " ," +
+			(height + margin.top - 45) + ")")
+		    .style("text-anchor", "middle")
+		    .text("Scattering Angle (degrees)");
+
+
+		// Build Y scales and axis:
+		var y = d3.scaleBand()
+		    .range([ height, 0 ])
+		    .domain(myVars)
+		    .padding(0.00);
+		    heatMapWrapper.append("g")
+		    .style("font-size", 15)
+		    .call(d3.axisLeft(y).tickSize(0))
+		    .select(".domain").remove();
+
+		    heatMapWrapper.append("text")
+		    .attr("transform", "rotate(-90)")
+		    .attr("y", 0 - margin.left)
+		    .attr("x",0 - (height / 2))
+		    .attr("dy", "1em")
+		    .style("text-anchor", "middle")
+		    .text("Shot Count");
+
+		// Build color scale
+		var myColor = d3.scaleSequential()
+		    .interpolator(d3.interpolateViridis)
+		    .domain([1,100]);
+
+		// create a tooltip
+		var tooltip = d3.select("#my_dataviz")
+		    .append("div")
+		    .style("opacity", 0)
+		    .attr("class", "tooltip")
+		    .style("background-color", "white")
+		    .style("border", "solid")
+		    .style("border-width", "2px")
+		    .style("border-radius", "5px")
+		    .style("padding", "5px")
+
+		// Three function that change the tooltip when user hover / move / leave a cell
+		var mouseover = function(d) {
+		    tooltip
+			.style("opacity", 1)
+		    d3.select(this)
+			.style("stroke", "white")
+			.style("opacity", 1)
+		};
+		var mousemove = function(d) {
+		    tooltip
+			.html("The raw intensity of<br/>this cell is: " + d.raw_intensity + "<br/>The raw scattering angle of<br/>this cell is: " +  d.raw_scattering_angle + " degrees")
+			.style("left", (d3.mouse(this)[0]+70) + "px")
+			.style("top", (d3.mouse(this)[1]) + "px")
+		};
+		var mouseleave = function(d) {
+		    tooltip
+			.style("opacity", 0)
+		    d3.select(this)
+			.style("stroke", "none")
+			.style("opacity", 1)
+		};
+
+		// add the squares
+		    heatMapWrapper.selectAll()
+		    .data(data, function(d) {return d.raw_scattering_angle + ':' + d.y_position;})
+		    .enter()
+		    .append("rect")
+		    .attr("x", function(d) { return x(d.raw_scattering_angle) })
+		    .attr("y", function(d) { return y(d.y_position) })
+		    .attr("rx", 0)
+		    .attr("ry", 0)
+		    .attr("width", x.bandwidth() + 1)
+		    .attr("height", y.bandwidth() + 1 )
+		    .style("fill", function(d) { return myColor(d.normalized_intensity)} )
+		    .style("stroke-width", 4)
+		    .style("stroke", "none")
+		    .style("opacity", 1)
+		    .on("mouseover", mouseover)
+		    .on("mousemove", mousemove)
+		    .on("mouseleave", mouseleave)
+		});
+
+		// Add title to graph
+		this.heatMapWrapper.append("text")
+		.attr("x", 0)
+		.attr("y", -50)
+		.attr("text-anchor", "left")
+		.style("font-size", "22px")
+		.text("XRD Data Contour Map");
+
+		// Add subtitle to graph
+		this.heatMapWrapper.append("text")
+		.attr("x", 0)
+		.attr("y", -20)
+		.attr("text-anchor", "left")
+		.style("font-size", "14px")
+		.style("fill", "grey")
+		.style("max-width", 400)
+		.text("This produces in Javascript with D3, the same visualization that Blake Sturtevant did in Origin");
+	}
+
 }
+
 
 //Set the csv file to use image.
 //HeatMapDisplay.prototype.setCSV = function(url) {
@@ -55,141 +191,6 @@ function HeatMapDisplay(parent, getData) {
 //}
 
 //Read the data
-HeatMapDisplay.prototype.create = function(heatMapWrapper) {
-	var myGroups;
-	var myVars;
-	this.heatMapWrapper = heatMapWrapper;
-	var csvFile = this.getData(0, "FILE_heatmap_path")[0];
-	d3.csv(csvFile, function(data){
-	myGroups = d3.map(data, function(d){
-		return d.raw_scattering_angle;
-	}).keys();
-	myVars = d3.map(data, function(d){
-		return d.y_position;
-	}).keys();
-
-	// Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
-
-	//this.heatMapWrapper = this.displays[5].heatMapWrapper;
-	var heatMapWrapper = this.heatMapWrapper;
-
-	var width = this.internalWidth;
-	var height = this.internalHeight;
-	var margin = this.margin;
-		
-		// Build X scales and axis:
-	var x = d3.scaleBand()
-	    .range([ 0, width ])
-	    .domain(myGroups)
-	    .padding(0.00);
-	    heatMapWrapper.append("g")
-	    .style("font-size", 15)
-	    .attr("transform", "translate(0," + height + ")")
-	    //keep every hundredth tick mark, and include the first and last.
-	    .call(d3.axisBottom(x).tickValues(x.domain().filter(function(d,i){return !(i%100) || i === x.domain().length - 1})).tickSize(6))
-	    .select(".domain").remove();
-	    heatMapWrapper.append("text")
-	    .attr("transform",
-		"translate(" + (width/2) + " ," +
-		(height + margin.top - 45) + ")")
-	    .style("text-anchor", "middle")
-	    .text("Scattering Angle (degrees)");
-
-
-	// Build Y scales and axis:
-	var y = d3.scaleBand()
-	    .range([ height, 0 ])
-	    .domain(myVars)
-	    .padding(0.00);
-	    heatMapWrapper.append("g")
-	    .style("font-size", 15)
-	    .call(d3.axisLeft(y).tickSize(0))
-	    .select(".domain").remove();
-
-	    heatMapWrapper.append("text")
-	    .attr("transform", "rotate(-90)")
-	    .attr("y", 0 - margin.left)
-	    .attr("x",0 - (height / 2))
-	    .attr("dy", "1em")
-	    .style("text-anchor", "middle")
-	    .text("Shot Count");
-
-	// Build color scale
-	var myColor = d3.scaleSequential()
-	    .interpolator(d3.interpolateViridis)
-	    .domain([1,100]);
-
-	// create a tooltip
-	var tooltip = d3.select("#my_dataviz")
-	    .append("div")
-	    .style("opacity", 0)
-	    .attr("class", "tooltip")
-	    .style("background-color", "white")
-	    .style("border", "solid")
-	    .style("border-width", "2px")
-	    .style("border-radius", "5px")
-	    .style("padding", "5px")
-
-	// Three function that change the tooltip when user hover / move / leave a cell
-	var mouseover = function(d) {
-	    tooltip
-		.style("opacity", 1)
-	    d3.select(this)
-		.style("stroke", "white")
-		.style("opacity", 1)
-	};
-	var mousemove = function(d) {
-	    tooltip
-		.html("The raw intensity of<br/>this cell is: " + d.raw_intensity + "<br/>The raw scattering angle of<br/>this cell is: " +  d.raw_scattering_angle + " degrees")
-		.style("left", (d3.mouse(this)[0]+70) + "px")
-		.style("top", (d3.mouse(this)[1]) + "px")
-	};
-	var mouseleave = function(d) {
-	    tooltip
-		.style("opacity", 0)
-	    d3.select(this)
-		.style("stroke", "none")
-		.style("opacity", 1)
-	};
-
-	// add the squares
-	    heatMapWrapper.selectAll()
-	    .data(data, function(d) {return d.raw_scattering_angle + ':' + d.y_position;})
-	    .enter()
-	    .append("rect")
-	    .attr("x", function(d) { return x(d.raw_scattering_angle) })
-	    .attr("y", function(d) { return y(d.y_position) })
-	    .attr("rx", 0)
-	    .attr("ry", 0)
-	    .attr("width", x.bandwidth() + 1)
-	    .attr("height", y.bandwidth() + 1 )
-	    .style("fill", function(d) { return myColor(d.normalized_intensity)} )
-	    .style("stroke-width", 4)
-	    .style("stroke", "none")
-	    .style("opacity", 1)
-	    .on("mouseover", mouseover)
-	    .on("mousemove", mousemove)
-	    .on("mouseleave", mouseleave)
-	});
-
-	// Add title to graph
-	heatMapWrapper.append("text")
-	.attr("x", 0)
-	.attr("y", -50)
-	.attr("text-anchor", "left")
-	.style("font-size", "22px")
-	.text("XRD Data Contour Map");
-
-	// Add subtitle to graph
-	heatMapWrapper.append("text")
-	.attr("x", 0)
-	.attr("y", -20)
-	.attr("text-anchor", "left")
-	.style("font-size", "14px")
-	.style("fill", "grey")
-	.style("max-width", 400)
-	.text("This produces in Javascript with D3, the same visualization that Blake Sturtevant did in Origin");
-}
 
 //Call this whenever the size of the parent changes. Redraws image to fit new size
 //Resets zoom
